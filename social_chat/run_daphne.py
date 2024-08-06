e9d0658992821a10ee8c9b75d358c11550e91b9d
souravchakraborty700 <souravchakraborty700@gmail.com>
2024-08-06 21:41:19 +0530

    cleaned directory

--- social_chat/run_daphne.py
+++ social_chat/run_daphne.py
@@ -1,26 +1,10 @@
-# import os
-# import django
-# from daphne.cli import CommandLineInterface
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_chat.settings')  # Ensure this matches your project structure
-
-# # Initialize Django
-# django.setup()
-
-# CommandLineInterface.entrypoint()
 import os
 import django
 from daphne.cli import CommandLineInterface
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_chat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_chat.settings')  # Ensure this matches your project structure
 
 # Initialize Django
 django.setup()
 
-# Use the PORT environment variable provided by Heroku
-port = os.environ.get('PORT', '8000')
-
-# Correct way to call the entrypoint
-CommandLineInterface().run(["daphne", "-b", "0.0.0.0", "-p", port, "social_chat.asgi:application"])
-
-
+CommandLineInterface.entrypoint()
