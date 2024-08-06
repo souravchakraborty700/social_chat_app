61462bdddb2c570d807fe74408d1883e445b2eb5
souravchakraborty700 <souravchakraborty700@gmail.com>
2024-08-06 08:01:09 +0530

    ready to deploy

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,29 +1,3 @@
-// // src/components/Header.js
-// import React, { useContext } from 'react';
-// import { Link } from 'react-router-dom';
-// import { AuthContext } from '../context/AuthContext';
-
-// const Header = () => {
-//   const { user } = useContext(AuthContext);
-
-//   return (
-//     <header className="header">
-//       <nav className="nav-logged-in">
-//         <ul>
-//           <li><Link to="/">Home</Link></li>
-//           <li><Link to="/connect">Friends</Link></li>
-//           <li><Link to="/users">People</Link></li>
-//           <li><Link to="/received-interests">Notifications</Link></li>
-//           <li><Link to="/logout">Logout</Link></li>
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-
 // src/components/Header.js
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
@@ -34,10 +8,6 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="greeting">
-        <p>Hello, {user ? user.username : 'Guest'}</p>
-        <p>Stay Connected With Your Friends</p>
-      </div>
       <nav className="nav-logged-in">
         <ul>
           <li><Link to="/">Home</Link></li>
@@ -52,3 +22,5 @@ const Header = () => {
 };
 
 export default Header;
+
+
